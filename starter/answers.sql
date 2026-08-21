389d0d4c2fc5665bf391e25ed3f065292eb0b522
CHANDRU-1U25CS015 <chandru94756@gmail.com>
2026-08-21 12:06:46 +0530

    Correct SQL commands in answers.sql
    Fix SQL syntax errors in table alterations and insert statement.

--- starter/answers.sql
+++ starter/answers.sql
@@ -10,7 +10,6 @@ DESC sTUDENT;
 SELECT * FROM Student;
 ALTER table Student and email VARCHAR (20);
 ALTER table Sudent and phone NUMBER INT(10);
-
 DESC Student;
 INSERT Student values("1020","Chandru","2008-04-09","Male","12201","chandru94756@gmail.com","6379062910");
 
